3c7d41dc45bd053d37e18dd77a73dfb99565a0f3
engrkashem <kashemaust@gmail.com>
2022-05-19 02:16:18 +0600

    post api is added and task can be saved to thr server

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.69xil.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const taskCollection = client.db('to-do-app').collection('tasks');
+
+        app.post('/task', async (req, res) => {
+            const taskInfo = req.body;
+            const result = await taskCollection.insertOne(taskInfo);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('To Do App Server is Running.')
